614ec851e23bba3c435643bf06a40a42f4e29586
Aditya Khandeshi <akhande3@jhu.edu>
2020-07-16 06:00:28 -0400

    Added docopt CLI and completed script

--- fastqcount.py
+++ fastqcount.py
@@ -4,21 +4,39 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-#learn how to use
-#import docopt
+from matplotlib.backends.backend_pdf import PdfPages
+from docopt import docopt
+usage = '''
+FastQ Data Analyzer
+
+Usage:
+    fastq.py <data> <results> <graphs> [<start_pos>[(- <end_pos>)]]...
+    fastq.py -h | --help
+    
+Arguments:
+    data    name of file or path to file containing fastq data
+    results name of or path to new xlsx file containing overall base counts and positional base frequencies if applicable
+    graphs  name of or path to new file containing barplot of total base counts and distribution of read length sizes
+    start_pos  either single base or starting position of range of positions for positional base frequencies to be displayed (enter individual bases after ranges!)
+    end_pos  end position of range of positions for positional base frequencies to be displayed 
+'''
+
+args = docopt(usage)
 
 reads = 0
 base_count = {"A": 0, "G": 0, "T": 0, "C": 0}
 position_count = {}
+positions = []
 read_lengths = []
 
 # Open file for reading and iterate through file reading every four lines and counting bases
-with open(sys.argv[1]) as fp:
+with open(args['<data>']) as fp:
     for line in fp:
         seq = fp.readline().rstrip()
         reads += 1
         fp.readline()
         qual = fp.readline()
+
         seqlen = len(seq)
         if next((len for len in read_lengths if len["Length"] == seqlen), None) not in read_lengths:
             read_lengths.append({"Length": seqlen, "Count": 1})
@@ -30,68 +48,63 @@ with open(sys.argv[1]) as fp:
                 position_count[pos] = {"A": 0, "G": 0, "T": 0, "C": 0}
             base_count[seq[pos]] += 1
             position_count[pos][seq[pos]] += 1
-
-
-# Print counts of each base and number of reads
-for base, count in base_count.items():
-    print("%s count: %d" % (base, count))
-print("Number of reads: %d" % reads)
 # Close file after reading
 fp.close()
 
-# Write base count data to csv file for storage
-fields = ["Base", "Count"]
-data = [["A", base_count['A']], ["G", base_count["G"]], ["T", base_count["T"]], ["C", base_count["C"]]]
-filename = "results.csv"
-with open(filename, 'w', newline='') as csv_file:
-    csv_writer = csv.writer(csv_file)
-    csv_writer.writerow(fields)
-    csv_writer.writerows(data)
-# Change base count data to a dataframe to plot as a barplot
+# Collects positions from command line (if applicable) and stores corresponding positional base frequencies as data frame
+for i in range(0, len(args['<start_pos>'])):
+    if args['<end_pos>'] and i < len(args["<end_pos>"]):
+        if args['<end_pos>'][i] == args["<start_pos>"][i]:
+            if args["<start_pos>"][i] not in positions:
+                positions.append(int(args["<start_pos>"][i]))
+        elif args['<end_pos>'][i] < args["<start_pos>"][i]:
+            r1, r2 = int(args["<end_pos>"][i]), int(args["<start_pos>"][i])
+            while r1 < r2 + 1:
+                if r1 not in positions:
+                    positions.append(r1)
+                r1 += 1
+        else:
+            r1, r2 = int(args["<start_pos>"][i]), int(args["<end_pos>"][i])
+            while r1 < r2 + 1:
+                if r1 not in positions:
+                    positions.append(r1)
+                r1 += 1
+    else:
+        if args["<start_pos>"][i] not in positions:
+            positions.append(int(args["<start_pos>"][i]))
+positions.sort()
+for pos in positions:
+    if pos < 0:
+        positions.remove(pos)
+
+# Trim position count dictionary to contain only desired positions
+for key in list(position_count.keys()):
+    if key not in positions:
+        del position_count[key]
+
+# Change base count data to a dataframe to plot as a barplot and write to xlsx file for storage
 bcount_dframe_dict = {"Base": ["A", "G", "T", "C"], "Count": [base_count["A"], base_count["G"], base_count["T"], base_count["C"]]}
 bcount_dframe = pd.DataFrame(bcount_dframe_dict)
-sns.barplot(x="Base", y="Count", data=bcount_dframe)
-plt.show()
+# Change position count data to a dataframe to write to xlsx file for storage
+pcount_dframe = pd.DataFrame(position_count)
+# Change number of reads to a dataframe to write to xlsx file for storage
+rcount_dframe = pd.DataFrame({"Number of Reads": reads}, index=[0])
 
-# Convert read length data to univariate list to plot as histogram
-# Look at something that can read bigger datasets; loop can use too much memory
-# Use a scatterplot?
+# Write data to xlsx file
+filename = args['<results>']
+with pd.ExcelWriter(filename) as writer:
+    rcount_dframe.to_excel(writer, sheet_name="Number of Reads")
+    bcount_dframe.to_excel(writer, sheet_name="Total Base Counts")
+    pcount_dframe.to_excel(writer, sheet_name="Positional Base Counts")
 
-sns.distplot(pd.DataFrame(read_lengths))
-import seaborn as sbn
 
-# Open file for reading
-reads = open(sys.argv[1], "r")
-# Skip first line to read bases
-reads.readline()
-line = reads.readline()
-base_count = {"A": 0, "G": 0, "T": 0, "C": 0}
-# Iterate through file reading every four lines and counting bases
-while line != "":
-    for x in range(len(line)):
-        if line[x] == "A":
-            base_count["A"] += 1
-        elif line[x] == "G":
-            base_count["G"] += 1
-        elif line[x] == "T":
-            base_count["T"] += 1
-        else:
-            base_count["C"] += 1
-    for x in range(3):
-        line = reads.readline()
-# Print counts of each base
-for base, count in base_count.items():
-    print("%s count: %d" % (base, count))
-# Close file after reading
-reads.close()
-# Write data to csv file for seaborn use
-fields = ["Base", "Count"]
-data = [["A", base_count['A']], ["G", base_count["G"]],["T", base_count["T"]], ["C", base_count["C"]]]
-filename = "results.csv"
-with open(filename, 'w') as csv_file:
-    csv_writer = csv.writer(csv_file)
-    csv_writer.writerow(fields)
-    csv_writer.writerows(data)
-data_set = pd.read_csv(filename)
-sbn.barplot(x="Base", y="Counts", data=data_set)
-plt.show()
+# Create total base count graphs and read length size distribution and store as pdf
+with PdfPages(args['<graphs>']) as pdf_pages:
+    figure1 = plt.figure(1)
+    barplot = sns.barplot(x="Base", y="Count", data=bcount_dframe)
+    barplot.set_title("Total Base Counts Among All Reads")
+    pdf_pages.savefig(figure1)
+    figure2 = plt.figure(2)
+    distribution = sns.lineplot(x="Length", y="Count", data=pd.DataFrame(read_lengths))
+    distribution.set_title("Distribution of Read Length Sizes")
+    pdf_pages.savefig(figure2)
