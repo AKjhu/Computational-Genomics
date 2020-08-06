c56b11b46f4d5d4b4854c913319280da8d9744e0
Aditya Khandeshi <akhande3@jhu.edu>
2020-08-06 12:54:34 -0500

    Removed unecessary for loop to increase efficiency

--- BAMComparison.py
+++ BAMComparison.py
@@ -6,30 +6,27 @@ transcriptome = pysam.AlignmentFile("/home/sunnyk/GenomicsFiles/reads.1.sanitize
 #genome = pysam.AlignmentFile("/home/sunnyk/GenomicsFiles/reads.1.sanitize.noribo.toGenome.sorted.bam", "rb")
 #result = pysam.AlignmentFile("/home/sunnyk/GenomicsFiles/results.bam", "wb", template=genome)
 
-# Create dicctionary and store all query names from genome file into dictionary as keys with value 0
+# Create dictionary and store all query names from transcriptome file into dictionary as keys with value 0
 t_qname = {}
 for x in transcriptome:
     query = x.query_name
     t_qname[query] = 0
 transcriptome.close()
 
-# Check each genome query name to verify if it is in the transcriptome; if found, increase its value to 1
+# Check each genome query name to verify if it is in the transcriptome; if not found, write read to output file
 genome = pysam.AlignmentFile("/home/sunnyk/GenomicsFiles/reads.1.sanitize.noribo.toGenome.sorted.bam", "rb")
 result = pysam.AlignmentFile("/home/sunnyk/GenomicsFiles/results.bam", "wb", template=genome)
 for read in genome:
-    for key in t_qname:
-        if read.query_name == key:
-            #result.write(read)
-            # g_qname[key] += 1
-            break
-    result.write(read)
+    if read.query_name not in t_qname:
+        result.write(read)
+        # g_qname[key] += 1
 # transcriptome.close()
 
 # Loop through dictionary and create new bam file containing reads not found in transcriptome
 # result = pysam.AlignmentFile("/home/sunnyk/GenomisFiles/results.bam", "wb", template=genome)
 # for read in genome:
-  #  if g_qname[read.query_name] == 0:
-   #     result.write(read)
+#  if g_qname[read.query_name] == 0:
+#     result.write(read)
 
 
 genome.close()
